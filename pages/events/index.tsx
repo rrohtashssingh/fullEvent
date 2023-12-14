23121dc27d2dd1d613e0573e6539f5a16065c7ff
am101 <ahmedsurya1000@gmail.com>
2023-12-15 00:32:51 +0530

    event tabs and table for list

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,3 +1,43 @@
+import Table from "@/components/util/Table";
+import Tab from "@/components/util/Tab";
 export default function Page() {
-  return <h1>Event Route</h1>;
+  const header = ["Event Name", "Description", "Date", "Created By"];
+  const tabs = ["All Events", "My Events", "Registered Events"];
+  const data = [
+    {
+      "Event Name": "abc",
+      Description: 1,
+      Date: "2023-12-10",
+      "Created By": "Ahmed",
+    },
+    {
+      "Event Name": "abc",
+      Description: 1,
+      Date: "2023-12-10",
+      "Created By": "Ahmed",
+    },
+    {
+      "Event Name": "abc",
+      Description: 1,
+      Date: "2023-12-10",
+      "Created By": "Ahmed",
+    },
+    {
+      "Event Name": "abc",
+      Description: 1,
+      Date: "2023-12-10",
+      "Created By": "Ahmed",
+    },
+  ];
+  return (
+    <>
+      <h1>Event Route</h1>
+      <div className="p-4">
+        <Tab data={tabs} />
+      </div>
+      <div className="p-4">
+        <Table data={data} header={header} />
+      </div>
+    </>
+  );
 }
